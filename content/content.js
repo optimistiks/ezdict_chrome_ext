e176f0263cb71d211338dd0d61894f9dfd671107
optimistiks <mail@potapovmax.com>
2015-07-05 15:52:08 +0400

    tooltip over selection prototype

--- content/content.js
+++ content/content.js
@@ -1,19 +1,48 @@
-var getSelectedText = function () {
-  var userSelection = window.getSelection();
-  return userSelection.toString().replace(/^\s+|\s+$/g, '');
+var contentApp = {
+  tooltip: null
 };
 
-document.addEventListener('mouseup', function () {
-  var text = getSelectedText();
+contentApp.createTooltip = function () {
+  return $('<div>').css({'position': 'fixed', 'background': 'red'}).html('some loader');
+};
+
+contentApp.showTooltip = function (selection) {
+  if (!this.tooltip) {
+    this.tooltip = this.createTooltip();
+  }
+
+  var range = selection.getRangeAt(0); //get the text range
+  var rect = range.getBoundingClientRect();
+
+  this.tooltip.css({top: rect.top - rect.height, left: rect.left});
+  this.tooltip.appendTo(document.body);
+
+  console.log('contentApp.showTooltip', 'show tooltip with loader at', rect);
+};
+
+contentApp.setTooltipContent = function () {
+  console.log('contentApp.setTooltipContent', 'set tooltip content');
+};
+
+// mouseuo
+// if tooltip not exist create tooltip
+// set tooltip content as loader
+// put tooltip to coordinates
+// change tooltip contents to translation when done
+
+$(document).on('mouseup', function (e) {
+  var selection = window.getSelection();
+  var text = selection.toString().replace(/^\s+|\s+$/g, '');
   if (text && text.length > 1) {
-      console.log(text);
+    console.log(text);
     chrome.runtime.sendMessage({text: text});
+    contentApp.showTooltip(selection);
   }
 });
 
 chrome.runtime.onMessage.addListener(
   function (request, sender, sendResponse) {
     if (request.translation) {
-        console.log('translation received by content script, showing tooltip now');
+      contentApp.setTooltipContent(request.translation);
     }
   });

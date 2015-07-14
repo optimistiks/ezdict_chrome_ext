25d10a5fd9e791b2b6850f8ebd37f71919d2d06b
Potapov Max <mp@multiship.ru>
2015-07-14 18:59:16 +0300

    new tooltip pt2

--- content/content.js
+++ content/content.js
@@ -1,76 +1,19 @@
-var contentApp = {
-  tooltip: null
-};
-
-contentApp.createTooltip = function () {
-  return $('<div>').css({
-    'position': 'absolute',
-    'background': 'red',
-    'max-width': '500px'
-  });
-};
-
-contentApp.showTooltip = function () {
-  if (!this.tooltip) {
-    this.tooltip = this.createTooltip();
-    this.tooltip.appendTo(document.body);
-  }
-  this.tooltip.show();
-  this.tooltip.html('loading...');
-  this.updateTooltipPosition();
-};
-
-contentApp.hideTooltip = function () {
-  if (!this.tooltip) {
-    return false;
-  }
-  this.tooltip.hide();
-};
-
-contentApp.setTooltipContent = function (translation) {
-  this.tooltip.text(translation);
-  this.updateTooltipPosition();
-};
-
-contentApp.updateTooltipPosition = function () {
-  var selection = window.getSelection();
-  var range = selection.getRangeAt(0); //get the text range
-  var rect = range.getBoundingClientRect();
-  this.tooltip.css({
-    top: window.pageYOffset + rect.top - this.tooltip.height(),
-    left: rect.left + rect.width / 2 - this.tooltip.width() / 2
-  });
-};
-
-// mouseuo
-// if tooltip not exist create tooltip
-// set tooltip content as loader
-// put tooltip to coordinates
-// change tooltip contents to translation when done
-
 $(document).on('mouseup', function (e) {
   var selection = window.getSelection();
   var text = selection.toString().replace(/^\s+|\s+$/g, '');
   if (text && text.length > 1) {
     console.log(text);
     chrome.runtime.sendMessage({text: text});
-    contentApp.showTooltip();
+    tooltip.showTooltip();
   } else {
-    contentApp.hideTooltip();
+    // tooltip.hideTooltip();
   }
-});
-
-$(document).ready(function () {
-  console.log('tooltip.html');
-  $.get(chrome.extension.getURL('content/tooltip/tooltip.html'), function (data) {
-    console.log("Load was performed.", data);
-    $(data).appendTo(document.body);
-  });
 });
 
 chrome.runtime.onMessage.addListener(
   function (request, sender, sendResponse) {
     if (request.translation) {
-      contentApp.setTooltipContent(request.translation);
+      tooltip.setTooltipContent(request.translation);
+      tooltip.updateTooltipPosition();
     }
   });
---
+++ content/tooltip/js/tooltip.js
@@ -0,0 +1,115 @@
+var tooltip = {
+  rootElem: null
+};
+
+tooltip.createTooltip = function () {
+  var deferred = new $.Deferred();
+
+  $.get(chrome.extension.getURL('content/tooltip/tooltip.html'), function (data) {
+    this.rootElem = $(data);
+    this.rootElem.appendTo(document.body);
+    this.bindEvents();
+    deferred.resolve(this.rootElem);
+  }.bind(this));
+
+  return deferred.promise();
+};
+
+/**
+ * todo: refactor
+ */
+tooltip.bindEvents = function () {
+  $('.st-translate').slimScroll({
+    distance: '5px',
+    height: '100%',
+    size: '4px'
+  });
+
+  $('.item-annotation-inner').slimScroll({
+    height: '100%',
+    size: '4px'
+  });
+
+  $('.pop-new-item').on('mouseenter', function () {
+    $(this).find('.item-annotation').stop().animate({'height': '100%'}, 'fast', function () {
+      $(this).find('.item-description-wrap').stop().fadeIn('fast').css('display', 'table');
+    });
+  });
+
+  $('.pop-new-item').on('mouseleave', function () {
+    $(this).find('.item-description-wrap').css('display', 'none');
+    $(this).find('.item-annotation').stop().animate({'height': '40px'}, 'fast');
+  });
+
+  $('.slide-btn').on('click', function () {
+    $('.slide-btn').removeClass('active');
+    $(this).addClass('active');
+    $('.carousel-item').addClass('next').stop().fadeOut(400, function () {
+      $(this).removeClass('next')
+    });
+    var index = $(this).index();
+    $('.carousel-item').eq(index).stop().fadeIn();
+  });
+
+  $(".slide-down-btn").on('click', function () {
+    if (!$('.st-translate').hasClass('large')) {
+      $(this).toggleClass('slide-up');
+      $('.st-translate').toggleClass('large');
+      $('.like-it').stop().fadeToggle();
+    } else {
+      $(this).toggleClass('slide-up');
+      $('.st-translate').toggleClass('large').scrollTop(0);
+      $('.like-it').stop().fadeToggle('fast');
+    }
+  });
+
+  $('.st-source-btn').on('click', function () {
+    if (!$(this).hasClass('active')) {
+      $('.st-source-btn').removeClass('active');
+      $(this).addClass('active');
+      var index = $(this).index();
+      $('.st-source-translate').stop().fadeOut(200).promise().done(function () {
+        $('.st-translate').scrollTop(0);
+        $('.st-source-translate').eq(index).stop().fadeIn(200);
+      });
+      $('.sticker').find('.slimScrollBar').animate({'top': '0'}, 300);
+    }
+  });
+
+  $('#close-sticker').on('click', function () {
+    $('.sticker').hide();
+  });
+};
+
+tooltip.showTooltip = function () {
+  if (!this.rootElem) {
+    this.createTooltip().done(function () {
+      this.showTooltip();
+    }.bind(this));
+  } else {
+    this.rootElem.show();
+    this.setTooltipContent('loading...');
+    this.updateTooltipPosition();
+  }
+};
+
+tooltip.hideTooltip = function () {
+  if (!this.rootElem) {
+    return false;
+  }
+  this.rootElem.hide();
+};
+
+tooltip.setTooltipContent = function (content) {
+  this.rootElem.find('#ezdict-sticker').text(content);
+};
+
+tooltip.updateTooltipPosition = function () {
+  var selection = window.getSelection();
+  var range = selection.getRangeAt(0); //get the text range
+  var rect = range.getBoundingClientRect();
+  this.rootElem.css({
+    top:  window.pageYOffset + rect.top - (this.rootElem.height() + 40),
+    left: rect.left + rect.width / 2 - this.rootElem.width() / 2
+  });
+};
